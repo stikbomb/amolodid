60d83bf9bee4d7a6ee2b9fdad7c60acad4788e8b
stikbomb <a.molodid@gmail.com>
2017-07-26 00:07:21 +0300

    JavaDoc. [task 4086]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Alexander Molodid (a.molodid@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
+	 /**
+     * Main.
+     * @param args - args.
+     */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
