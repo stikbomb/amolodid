bda20b3471c1c59d1582f9ce480dc171e1afb9dc
stikbomb <a.molodid@gmail.com>
2017-07-26 10:15:59 +0300

    1.4 JUnit [task #4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -13,6 +13,6 @@ public class Calculate {
      * @param args - args.
      */
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello world");
 	}
 }
