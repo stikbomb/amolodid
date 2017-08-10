e6b2e9f261a391385387432c4e73c0e87a339890
stikbomb <a.molodid@gmail.com>
2017-08-10 11:17:12 +0300

    4.4. Построить пирамиду в псевдографике [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,40 @@
+package ru.job4j.loop;
+
+/**
+ * Построение пирамиды по заданной высоте
+ *
+ * @author Alexander Molodid (a.molodid@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Paint {
+    StringBuilder builder = new StringBuilder();
+
+    public String piramid(int h) {
+
+        for (int i = 1; i <= h; i++) {
+
+            //цикл для построения отступов слева
+            for (int s1 = h - i; s1 > 0; s1--) {
+                builder.append(" ");
+            }
+
+            //цикл для построения "тела" пирамиды
+            for (int k = 1; k <= (i * 2 - 1); k++) {
+                builder.append("^");
+            }
+
+            //цикл для построения отступов справа
+            for (int s2 = h - i; s2 > 0; s2--) {
+                builder.append(" ");
+            }
+
+            //перенос каретки после построения строки
+            builder.append(System.getProperty("line.separator"));
+        }
+
+        //возврат построенной пирамиды
+        return builder.toString();
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PaintTest {
+    @Test
+    public void whenPiramidWithHeightTwoThenStringWithTwoRows() {
+        Paint paint = new Paint();
+        String result = paint.piramid(2);
+        String expected = String.format(" ^ %s^^^%s", System.getProperty("line.separator"),
+                System.getProperty("line.separator"));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenPiramidWithHeightThreeThenStringWithThreeRows() {
+        Paint paint = new Paint();
+        String result = paint.piramid(3);
+        String expected = String.format("  ^  %s ^^^ %s^^^^^%s", System.getProperty("line.separator"),
+                System.getProperty("line.separator"), System.getProperty("line.separator"));
+        assertThat(result, is(expected));
+    }
+}
